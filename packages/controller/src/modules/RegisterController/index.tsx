753fc69618cdaf8d9e2922f1d8e2280b5b7274e8
paul stepanenko <stepanenko.p@nimses.com>
2019-04-04 17:42:45 +0300

    added register mutation

--- packages/controller/src/modules/RegisterController/index.tsx
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
+import { graphql, ChildMutateProps } from 'react-apollo';
+import gql from 'graphql-tag';
 
 interface Props {
   children: (data: { submit: (values: any) => Promise<null> }) => React.ReactNode | null;
 }
 
-export class RegisterController extends React.PureComponent<Props> {
+class C extends React.PureComponent<ChildMutateProps<Props, any, any>> {
   submit = async (values: any) => {
     console.log(values);
+    const result = await this.props.mutate({
+      variables: values
+    })
+    console.log(result);
     return null;
   }
 
@@ -14,3 +20,14 @@ export class RegisterController extends React.PureComponent<Props> {
     return this.props.children({ submit: this.submit });
   }
 }
+
+const registerMutation = gql`
+  mutation($email: String!, $password: String!) {
+    register(email:$email, password:$password) {
+      path
+      message
+    }
+  }
+`;
+
+export const RegisterController = graphql(registerMutation)(C);
