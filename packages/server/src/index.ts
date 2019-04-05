da76fb7ce35142df67465480b93ed9928ec76b92
paul stepanenko <stepanenko.p@nimses.com>
2019-04-05 19:11:57 +0300

    deployed to digital ocean and dokku

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,4 @@
 import { startServer } from "./startServer";
 
+console.log(process.env.NODE_ENV)
 startServer();
--- packages/server/src/utils/createTypeormConn.ts
+++ packages/server/src/utils/createTypeormConn.ts
@@ -1,6 +1,15 @@
 import { getConnectionOptions, createConnection } from "typeorm";
+import { User } from '../entity/User';
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection({ ...connectionOptions, name: "default" });
+  console.log(connectionOptions)
+  return process.env.NODE_ENV === 'production'
+    ? createConnection({
+      ...connectionOptions,
+      entities: [User],
+      url: process.env.DATABASE_URL as string,
+      name: "default"
+    } as any)
+    : createConnection({ ...connectionOptions, name: "default" });
 };
