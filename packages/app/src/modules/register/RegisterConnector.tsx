680099afbfbfd642d81b2926c83a40bdade9b9ab
paul stepanenko <stepanenko.p@nimses.com>
2019-04-10 15:28:45 +0300

    fixed native app build

---
+++ packages/app/src/modules/register/RegisterConnector.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Button } from 'react-native-elements';
+
+export class RegisterConnector extends React.PureComponent {
+  
+  onPress = () => {
+    console.log('button pressed');
+  }
+  
+  render() {
+    return (
+      <Button title="BUTTON" onPress={this.onPress} />
+    );
+  }
+}
